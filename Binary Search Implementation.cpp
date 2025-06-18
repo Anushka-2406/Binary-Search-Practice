7490703a9be682da1cb3d83fd0d74088ca073e17
Anushka-2406 <bansalanushka2406@gmail.com>
2025-06-18 14:31:41 +0530

    Add files via upload

---
+++ Binary Search Implementation.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> arr={1,2,3,5,7};
+    int x=5;
+    int l=0;
+    int r=4;
+    int ans=0;
+    int mid;
+    while(l<=r){
+        mid=(l+r)/2;
+        if(arr[mid]==x){
+            ans=mid;
+            break;
+        }
+        else if(arr[mid]>x) r=mid-1;
+        else l=mid+1;
+        
+        cout<<l<<" "<<r<<endl;
+    }
+    cout<<ans;
+
+    return 0;
+}
